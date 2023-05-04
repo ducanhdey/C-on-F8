8b1b0cff60e0774f4591ba1e8594dae37c870b0f
Phan Duc Anh <114552003+ducanhdey@users.noreply.github.com>
2023-05-04 08:52:54 +0700

    chinh sua lai chut

--- .vscode/baitap1.cpp
+++ .vscode/baitap1.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main () {
-    int a,b;
-    cout <<"nhap vao so a ";
-    cin >> a ;
-    cout <<"\nnhap vao so b ";
+int main()
+{
+    int a, b;
+    cout << "nhap vao so a ";
+    cin >> a;
+    cout << "\nnhap vao so b ";
     cin >> b;
-    cout << "a + b = " << a + b ;
+    cout << "a + b = " << a + b;
     return 0;
 }
